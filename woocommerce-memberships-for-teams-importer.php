f14903075c4aa8a8846b603f3b6e8aa22dcd7aff
Bradford Knowlton <brad@ucanbonline.net>
2023-08-23 21:24:06 -0600

    initial commit

---
+++ woocommerce-memberships-for-teams-importer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       WooCommerce Membership For Teams Importer
+ * Plugin URI:        https://github.com/GrowDesign/woocommerce-memberships-for-teams-importer
+ * Description:       Handle the additional code needed to customize WooCommerce Membership for Teams
+ * Version:           2023.8.23
+ * Requires at least: 6.0
+ * Requires PHP:      7.2
+ * Author:            Bradford Knowlton
+ * Author URI:        https://bradknowlton.com/
+ * License:           BSD 3-clause
+ * License URI:       https://opensource.org/licenses/BSD-3-Clause
+ * Domain Path:       /languages
+ */
+ 
+ 
