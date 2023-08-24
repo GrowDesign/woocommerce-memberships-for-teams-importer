0cf49fb2a7ac8fa923197b41203fed28a18cef3f
Bradford Knowlton <brad@ucanbonline.net>
2023-08-23 22:25:27 -0600

    working commit

--- woocommerce-memberships-for-teams-importer.php
+++ woocommerce-memberships-for-teams-importer.php
@@ -13,4 +13,41 @@
  * Domain Path:       /languages
  */
  
- 
+ /**
+ * Filter CSV User Membership import data before processing an import.
+ *
+ * @since 1.6.0
+ *
+ * @param array $import_data the imported data as associative array
+ * @param string $action either 'create' or 'merge' (update) a User Membership
+ * @param array $columns CSV columns raw data
+ * @param array $row CSV row raw data
+ * @param \stdClass $job import job
+ */
+function wc_memberships_csv_import_user_memberships_for_teams_data( $import_data, $action, $columns, $row, $job ) {
+	global $team_id;
+	if(isset($row['team_id']) && is_numeric($row['team_id']) && 0 < $row['team_id'] ){
+		$team_id = $row['team_id'];
+	}
+    return $import_data;
+}
+add_filter( 'wc_memberships_csv_import_user_memberships_data', 'wc_memberships_csv_import_user_memberships_for_teams_data', 10, 5 );
+
+/**
+ * Fires upon creating or updating a User Membership from import data.
+ *
+ * @since 1.6.0
+ *
+ * @param \WC_Memberships_User_Membership $user_membership User Membership object
+ * @param string $action either 'create' or 'merge' (update) a User Membership
+ * @param array $data import data
+ * @param \stdClass $job import job
+ */
+function wc_memberships_csv_import_user_membership_for_teams( $user_membership, $action, $import_data, $job ) {
+	global $team_id;
+	if ( 0 < $team_id ){
+		$team = wc_memberships_for_teams_get_team( $team_id );
+		$team->add_member( $import_data['user_id'] );
+	}
+}
+add_filter( 'wc_memberships_csv_import_user_membership', 'wc_memberships_csv_import_user_membership_for_teams', 10, 4 );
